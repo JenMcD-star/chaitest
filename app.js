47aa10bf228b10de565ab56c9e05bd2246edafb3
JenMcD-star <jenniferannvancura@gmail.com>
2023-03-20 11:45:45 -0500

    Save person and return list of people.

--- app.js
+++ app.js
@@ -5,11 +5,47 @@ app.use(express.json());
 
 const people = [];
 
+app.post("/api/v1/people", (req, res) => {
+  if (!req.body.name) {
+    res.status(400).json({ error: "A name is required." });
+    return;
+  }
+  if (!req.body.age) {
+    res.status(400).json({ error: "An age is required." });
+    return;
+  }
+  const age = Number(req.body.age);
+  if (age < 0) {
+    res.status(400).json({ error: "Must be a valid age." });
+    return;
+  }
+  req.body.age = age;
+  req.body.index = people.length;
+  people.push(req.body);
 
-app.all("/api/v1/*", (req, res) => {
-  res.json({ error: "That route is not implemented." });
+  res.status(201).json(`${req.body.name}, age: ${req.body.age} index number ${req.body.index} added.`);
 });
+
+app.get("/api/v1/people", (req, res) => {
+  res.json(people);
+});
+
+
+
+//app.all("/api/v1/*", (req, res) => {
+// res.json({ error: "That route is not implemented." });
+//});
 
 const server = app.listen(3000, () => {
   console.log("listening on port 3000...");
 });
+
+
+module.exports = { app, server }
+
+/*
+, retrieving the list of
+people entries, and retrieving a single person entry. People entries are stored in
+the people array, which starts out empty. They are not persisted. There is no
+file i/o or database access in this exercise.
+*/
